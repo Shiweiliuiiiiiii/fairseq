7c2c8b7f4112a6113cbd721e2e396908150aee2e
Shiweiliuiiiiiii <s.liu3@tue.nl>
2022-08-02 16:01:39 +0200

    fix IMP bug

--- fairseq_cli/sparse_core.py
+++ fairseq_cli/sparse_core.py
@@ -196,6 +196,7 @@ class Masking(object):
                 self.name2nonzeros[name] = (weight != 0).sum().item()
                 total_num_nonzoros += self.name2nonzeros[name]
                 dense_nonzeros += weight.numel()
+                print(f'sparsity of layer {name} is {self.name2nonzeros[name]/weight.numel()}')
 
             print(f'sparsity level of current model is {1-total_num_nonzoros/dense_nonzeros}')
 
