e5d5b4d653a4504bec6cf0588121a3696849eb71
刘世伟 <liushiwei@v099140.vpn.tue.nl>
2022-09-05 14:29:57 -0500

    snip rigl test qa

--- examples/roberta/commonsense_qa/evaluate.py
+++ examples/roberta/commonsense_qa/evaluate.py
@@ -13,7 +13,7 @@ def sorted_nicely(l):
 
 check_point_folder_source = '/projects/0/prjste21060/projects/pruning_fails/QA/robert/commonsenseqa/'
 
-for method in ['snip_rigl']:
+for method in ['snip_rigl/']:
 
     check_point_folder = check_point_folder_source + method
     # model_files = [0.5]
