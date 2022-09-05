87f5ac20dd434c4f6e7e57c99e380eea80db11b8
刘世伟 <liushiwei@v099140.vpn.tue.nl>
2022-09-05 15:22:36 -0500

    race test after

--- plot/Commonsense_qa.py
+++ plot/Commonsense_qa.py
@@ -3,14 +3,15 @@ import numpy as np
 from matplotlib.pyplot import figure
 fig = figure(num=None, figsize=(8, 5), dpi=150, facecolor='w', edgecolor='k')
 fontsize = 15
-markersize = 3
-
+markersize = 8
+linewidth = 2
 dense = [77.31]
 
 robert_lth = [75.92, 74.04, 69.62, 19.57, 19.57, 19.57, 19.57, 19.57, 19.57, 19.57]
 x_lth = [0.2, 0.36, 0.488, 0.590, 0.672, 0.738, 0.791, 0.8325, 0.866, 0.893]
 
 robert_snip = [20.31, 18.92, 18.51, 19.16, 18.67, 18.76, 18.76, 18.59, 18.59, 18.26]
+robert_snip_rigl = [19.00, 19.00, 20.22, 17.60, 22.19, 19.73, 18.75, 19.25, 20.14, 19.57]
 robert_gm_before = [75.92, 59.41, 39.39, 19.57, 19.57, 19.57, 19.57, 19.57, 19.57, 19.57]
 robert_gm_after = [62.24, 29.07, 20.63, 19.57, 19.57, 19.57, 19.57, 19.57, 19.57, 19.57]
 robert_gm_rigl = [60.36, 29.57, 21.38, 19.57, 19.57, 19.57, 19.57, 19.57, 19.57, 19.57]
@@ -23,22 +24,24 @@ x_axis = range(10)
 
 # prune
 roberta_large = fig.add_subplot(1,1,1)
-roberta_large.plot(x_axis, dense*10,  '-o',   label='Dense model',color='black',linewidth=3, markersize=markersize, )
-roberta_large.plot(x_axis, robert_snip,  '-',   label='SNIP',color='#dbb40c',linewidth=3, markersize=markersize, )
-roberta_large.plot(x_axis, robert_lth,  '-',   label='LTH',color='orange',linewidth=3, markersize=markersize, )
-roberta_large.plot(x_axis, robert_gm_rigl,  '-',   label='OMP+RIGL',color='red',linewidth=3, markersize=markersize, )
-roberta_large.plot(x_axis, robert_gm_before,  '-',   label='OMP Before',color='green',linewidth=3, markersize=markersize, )
-roberta_large.plot(x_axis, robert_gm_after,  '-',   label='OMP After',color='cyan',linewidth=3, markersize=markersize, )
-roberta_large.plot(x_axis, robert_random_before,  '-',   label='Random Before',color='purple',linewidth=3, markersize=markersize, )
-roberta_large.plot(x_axis, robert_random_rigl,  '-',   label='Random+RIGL',color='purple',linewidth=3, markersize=markersize, )
-roberta_large.plot(x_axis, robert_random_after,  '-',   label='Random After',color='yellow',linewidth=3, markersize=markersize, )
-# vgg_all.plot(x_axis, [50]*11,  '-o',   label='random guess',color='gray',linewidth=3, markersize=markersize, )
+roberta_large.plot(x_axis, dense*10,  '-o',   label='Dense model',color='black',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, robert_snip,  '-',   label='SNIP',color='blue',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, robert_snip_rigl,  '-',   label='SNIP+RIGL',color='blue',linewidth=linewidth, markersize=markersize, marker='^'  )
+roberta_large.plot(x_axis, robert_lth,  '-',   label='LTH',color='orange',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, robert_gm_before,  '-',   label='OMP Before',color='green',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, robert_gm_after,  '--',   label='OMP After',color='green',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, robert_gm_rigl,  '-',   label='OMP+RIGL',color='green',linewidth=linewidth, markersize=markersize, marker='^' )
+roberta_large.plot(x_axis, robert_random_before,  '-',   label='Random Before',color='purple',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, robert_random_after,  '--',   label='Random After',color='purple',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, robert_random_rigl,  '-',   label='Random+RIGL',color='purple',linewidth=linewidth, markersize=markersize, marker='^' )
+
+
 roberta_large.set_title('Roberta large on CommonsenseQA',fontsize=fontsize)
 roberta_large.set_xticks(range(10))
 roberta_large.set_xticklabels(np.array( [0.2, 0.36, 0.488, 0.590, 0.672, 0.738, 0.791, 0.8325, 0.866, 0.893]), fontsize=10 )
 
 roberta_large.set_ylabel('Accuracy', fontsize=fontsize)
-roberta_large.set_xlabel('sparsity',fontsize=fontsize)
+roberta_large.set_xlabel('Sparsity',fontsize=fontsize)
 plt.legend()
 plt.savefig('Roberta_CommonsenseQA.png')
 plt.show()
--- plot/winogrande.py
+++ plot/winogrande.py
@@ -4,7 +4,7 @@ from matplotlib.pyplot import figure
 fig = figure(num=None, figsize=(8, 5), dpi=150, facecolor='w', edgecolor='k')
 fontsize = 15
 markersize = 8
-
+linewidth =2
 dense = [77.31]
 
 acc_gm = []
@@ -27,6 +27,13 @@ with open('/Users/liushiwei/Projects/fairseq/results/reasoning/winogrande/winogr
     for line in file:
         if 'val | epoch 019 |' in line:
             acc_snip.append(float(line.split()[19][:-1]))
+
+acc_snip_rigl = []
+with open('/Users/liushiwei/Projects/fairseq/results/reasoning/winogrande/winogrande_snip_rigl.out') as file:
+    for line in file:
+        if 'val | epoch 019 |' in line:
+            acc_snip_rigl.append(float(line.split()[19][:-1]))
+
 
 acc_omp_rigl = []
 with open('/Users/liushiwei/Projects/fairseq/results/reasoning/winogrande/winogrande_gm_rigl.out') as file:
@@ -69,24 +76,24 @@ x_axis = range(10)
 
 # prune
 roberta_large = fig.add_subplot(1,1,1)
-roberta_large.plot(x_axis, acc_snip,  '-',   label='SNIP',color='blue',linewidth=3, markersize=markersize, )
-# roberta_large.plot(x_axis, acc_snip_rigl,  '-',   label='SNIP+RIGL',color='blue',linewidth=3, markersize=markersize, marker='^'  )
-roberta_large.plot(x_axis, acc_imp[1:],  '-',   label='LTH',color='orange',linewidth=3, markersize=markersize, )
-roberta_large.plot(x_axis, acc_gm,  '-',   label='OMP Before',color='green',linewidth=3, markersize=markersize, )
-roberta_large.plot(x_axis, acc_omg_after,  '--',   label='OMP After',color='green',linewidth=3, markersize=markersize, )
-roberta_large.plot(x_axis, acc_omp_rigl,  '-',   label='OMP+RIGL',color='green',linewidth=3, markersize=markersize, marker='^' )
-roberta_large.plot(x_axis, acc_random,  '-',   label='Random Before',color='purple',linewidth=3, markersize=markersize, )
-roberta_large.plot(x_axis, acc_random_after,  '--',   label='Random After',color='purple',linewidth=3, markersize=markersize, )
-roberta_large.plot(x_axis, acc_random_rigl,  '-',   label='Random+RIGL',color='purple',linewidth=3, markersize=markersize, marker='^' )
+roberta_large.plot(x_axis, acc_snip,  '-',   label='SNIP',color='blue',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, acc_snip_rigl,  '-',   label='SNIP+RIGL',color='blue',linewidth=linewidth, markersize=markersize, marker='^'  )
+roberta_large.plot(x_axis, acc_imp[1:],  '-',   label='LTH',color='orange',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, acc_gm,  '-',   label='OMP Before',color='green',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, acc_omg_after,  '--',   label='OMP After',color='green',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, acc_omp_rigl,  '-',   label='OMP+RIGL',color='green',linewidth=linewidth, markersize=markersize, marker='^' )
+roberta_large.plot(x_axis, acc_random,  '-',   label='Random Before',color='purple',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, acc_random_after,  '--',   label='Random After',color='purple',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, acc_random_rigl,  '-',   label='Random+RIGL',color='purple',linewidth=linewidth, markersize=markersize, marker='^' )
 
-roberta_large.plot(x_axis, [acc_imp[0]]*10,  '-o',   label='Dense model',color='black',linewidth=3, markersize=markersize, )
+roberta_large.plot(x_axis, [acc_imp[0]]*10,  '-o',   label='Dense model',color='black',linewidth=linewidth, markersize=markersize, )
 # vgg_all.plot(x_axis, [50]*11,  '-o',   label='random guess',color='gray',linewidth=3, markersize=markersize, )
 roberta_large.set_title('Roberta Large on WinoGrande',fontsize=fontsize)
 roberta_large.set_xticks(range(10))
 roberta_large.set_xticklabels(np.array( [0.2, 0.36, 0.488, 0.590, 0.672, 0.738, 0.791, 0.8325, 0.866, 0.893]), fontsize=10 )
 
 roberta_large.set_ylabel('Accuracy', fontsize=fontsize)
-roberta_large.set_xlabel('sparsity',fontsize=fontsize)
+roberta_large.set_xlabel('Sparsity',fontsize=fontsize)
 plt.legend()
 plt.savefig('Roberta_WinoGrande.png')
 plt.show()
