4670d72ff04e40155b857daabf5008b26f5608fb
刘世伟 <liushiwei@v092252.vpn.tue.nl>
2022-09-15 23:01:25 -0500

    add seed23

--- plot/Commonsense_qa.py
+++ plot/Commonsense_qa.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.pyplot import figure
-fig = figure(num=None, figsize=(16, 9), dpi=150, facecolor='w', edgecolor='k')
-fontsize = 12
-Titlesize = 15
-markersize = 4
-linewidth = 1.5
+fig = figure(num=None, figsize=(16, 10), dpi=150, facecolor='w', edgecolor='k')
+fontsize = 14
+Titlesize = 18
+markersize = 7
+linewidth = 2.2
 
 
 # commonsenseQA
@@ -156,6 +156,25 @@ with open('/Users/liushiwei/Projects/fairseq/results/reasoning/race/cobert_race_
         if ' | test1 | valid on' in line:
             acc_test_snip_rigl_high_RACE.append(float(line.split()[19]))
 
+acc_test_snip_rigl_RACE = []
+acc_test_snip_rigl_high_RACE = []
+with open('/Users/liushiwei/Projects/fairseq/results/reasoning/race/cobert_race_snip_rigl_test.out') as file:
+    for line in file:
+        if 'test | valid on' in line:
+            acc_test_snip_rigl_RACE.append(float(line.split()[19]))
+        if ' | test1 | valid on' in line:
+            acc_test_snip_rigl_high_RACE.append(float(line.split()[19]))
+
+acc_test_omp_random_after_RACE = []
+acc_test_omp_random_after_high_RACE = []
+with open('/Users/liushiwei/Projects/fairseq/results/reasoning/race/cobert_race_omp_random_after_test.out') as file:
+    for line in file:
+        if 'test | valid on' in line:
+            acc_test_omp_random_after_RACE.append(float(line.split()[19]))
+        if ' | test1 | valid on' in line:
+            acc_test_omp_random_after_high_RACE.append(float(line.split()[19]))
+
+
 
 
 acc_test_imp_RACE = acc_test_RACE[:10]
@@ -171,11 +190,11 @@ acc_test_snip_high_RACE = acc_test1_RACE[20:29]
 acc_test_snip_high_RACE.append(25.0)
 
 # after
-# acc_test_random_after = acc_test_random_omp_after[:10]
-# acc_test_omp_after = acc_test_random_omp_after[10:]
+acc_test_random_after_RACE = acc_test_omp_random_after_RACE[10:]
+acc_test_omp_after_RACE =  acc_test_omp_random_after_RACE[:10]
 
-# acc_test_random_after_high = acc_test_random_omp_after_high[:10]
-# acc_test_omp_after_high = acc_test_random_omp_after_high[10:]
+acc_test_random_after_high_RACE = acc_test_omp_random_after_high_RACE[10:]
+acc_test_omp_after_high_RACE = acc_test_omp_random_after_high_RACE[:10]
 
 # rigl
 acc_test_random_rigl_RACE = acc_test_random_omp_rigl_RACE[:10]
@@ -193,10 +212,10 @@ roberta_large = fig.add_subplot(2,2,1)
 roberta_large.plot(x_axis, dense_csqa*10,  '-o',   label='Dense model',color='black',linewidth=linewidth, markersize=markersize, )
 
 
-roberta_large.plot(x_axis, robert_snip_csqa,  '-o',   label='SNIP (Before)',color='#00FF00',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, robert_snip_csqa,  '-o',   label='SNIP (Before)',color='#228B22',linewidth=linewidth, markersize=markersize, )
 roberta_large.plot(x_axis, robert_lth_csqa,  '-o',   label='LTH (After)',color='orange',linewidth=linewidth, markersize=markersize, )
 
-roberta_large.plot(x_axis, robert_snip_rigl_csqa,  '--o',   label='SNIP+RIGL (Before)',color='#00FF00',linewidth=linewidth, markersize=markersize )
+roberta_large.plot(x_axis, robert_snip_rigl_csqa,  '--o',   label='SNIP+RIGL (Before)',color='#228B22',linewidth=linewidth, markersize=markersize )
 roberta_large.plot(x_axis, robert_gm_after_csqa,  '-o',   label='One-Shot LRR (After)',color='#00BFFF',linewidth=linewidth, markersize=markersize, )
 
 
@@ -204,7 +223,7 @@ roberta_large.plot(x_axis, robert_random_before_csqa,  '-o',   label='Random (Be
 roberta_large.plot(x_axis, robert_random_after_csqa,  '-o',   label='Random LRR (After)',color='#0072BD',linewidth=linewidth, markersize=markersize, )
 
 roberta_large.plot(x_axis, robert_random_rigl_csqa,  '--o',   label='Random+RIGL (Before)',color='brown',linewidth=linewidth, markersize=markersize)
-roberta_large.plot(x_axis, robert_gmp_csqa,  '-o',   label='GMP (During)',color='magenta',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, robert_gmp_csqa,  '-o',   label='GMP (During)',color='#CD00CD',linewidth=linewidth, markersize=markersize, )
 
 roberta_large.plot(x_axis, robert_gm_before_csqa,  '-o',   label='OMP (Before)' ,color='#bcbd22',linewidth=linewidth, markersize=markersize, )
 roberta_large.plot(x_axis, robert_gm_rigl_csqa,  '--o',   label='OMP+RIGL (Before)',color='#bcbd22',linewidth=linewidth, markersize=markersize )
@@ -232,19 +251,19 @@ roberta_large.spines['top'].set_visible(False)
 
 
 roberta_large = fig.add_subplot(2,2,2)
-roberta_large.plot(x_axis, [acc_imp_wino[0]]*10,  '-o', color='black',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, acc_imp_wino[1:],  '-o', color='orange',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, acc_omg_after_wino,  '-o', color='#00BFFF',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, acc_random_after_wino,  '-o', color='#0072BD',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, acc_gmp_wino,  '-o', color='magenta',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, 100*np.array([acc_imp_wino[0]]*10),  '-o', color='black',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, 100*np.array(acc_imp_wino[1:]),  '-o', color='orange',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, 100*np.array(acc_omg_after_wino),  '-o', color='#00BFFF',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, 100*np.array(acc_random_after_wino),  '-o', color='#0072BD',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, 100*np.array(acc_gmp_wino),  '-o', color='#CD00CD',linewidth=linewidth, markersize=markersize, )
 
 
-roberta_large.plot(x_axis, acc_snip_wino,  '-o', color='#00FF00',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, acc_snip_rigl_wino,  '--o', color='#00FF00',linewidth=linewidth, markersize=markersize )
-roberta_large.plot(x_axis, acc_random_wino,  '-o', color='brown',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, acc_random_rigl_wino,  '--o', color='brown',linewidth=linewidth, markersize=markersize)
-roberta_large.plot(x_axis, acc_gm_wino,  '-o',  color='#bcbd22',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, acc_omp_rigl_wino,  '--o' ,color='#bcbd22',linewidth=linewidth, markersize=markersize )
+roberta_large.plot(x_axis, 100*np.array(acc_snip_wino),  '-o', color='#228B22',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, 100*np.array(acc_snip_rigl_wino),  '--o', color='#228B22',linewidth=linewidth, markersize=markersize )
+roberta_large.plot(x_axis, 100*np.array(acc_random_wino),  '-o', color='brown',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, 100*np.array(acc_random_rigl_wino),  '--o', color='brown',linewidth=linewidth, markersize=markersize)
+roberta_large.plot(x_axis, 100*np.array(acc_gm_wino),  '-o',  color='#bcbd22',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, 100*np.array(acc_omp_rigl_wino),  '--o' ,color='#bcbd22',linewidth=linewidth, markersize=markersize )
 
 
 roberta_large.set_title('Roberta on WinoGrande',fontsize=Titlesize)
@@ -271,13 +290,13 @@ roberta_large = fig.add_subplot(2,2,3)
 
 roberta_large.plot(x_axis, [acc_test_imp_RACE[0]]*10,  '-o', color='black',linewidth=linewidth, markersize=markersize, )
 roberta_large.plot(x_axis, acc_test_imp_RACE[1:],  '-o', color='orange',linewidth=linewidth, markersize=markersize, )
-# roberta_large.plot(x_axis, acc_test_omp_after_high,  '-o',   label='One-Shot LRR (After)',color='#00BFFF',linewidth=linewidth, markersize=markersize, )
-# roberta_large.plot(x_axis, acc_test_random_after_high,  '-o',   label='Random LRR (After)',color='#0072BD',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, acc_test_gmp_RACE,  '-o', color='magenta',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, acc_test_omp_after_RACE,  '-o',color='#00BFFF',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, acc_test_random_after_RACE,  '-o',color='#0072BD',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, acc_test_gmp_RACE,  '-o', color='#CD00CD',linewidth=linewidth, markersize=markersize, )
 
 
-roberta_large.plot(x_axis, acc_test_snip_RACE,  '-o', color='#00FF00',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, acc_test_snip_rigl_RACE,  '--o', color='#00FF00',linewidth=linewidth, markersize=markersize )
+roberta_large.plot(x_axis, acc_test_snip_RACE,  '-o', color='#228B22',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, acc_test_snip_rigl_RACE,  '--o', color='#228B22',linewidth=linewidth, markersize=markersize )
 roberta_large.plot(x_axis, acc_test_random_RACE,  '-o', color='brown',linewidth=linewidth, markersize=markersize, )
 roberta_large.plot(x_axis, acc_test_random_rigl_RACE, '--o', color='brown',linewidth=linewidth, markersize=markersize)
 roberta_large.plot(x_axis, acc_test_omp_RACE,  '-o', color='#bcbd22',linewidth=linewidth, markersize=markersize, )
@@ -308,13 +327,13 @@ roberta_large = fig.add_subplot(2,2,4)
 
 roberta_large.plot(x_axis, [acc_test_imp_high_RACE[0]]*10,  '-o', color='black',linewidth=linewidth, markersize=markersize, )
 roberta_large.plot(x_axis, acc_test_imp_high_RACE[1:],  '-o', color='orange',linewidth=linewidth, markersize=markersize, )
-# roberta_large.plot(x_axis, acc_test_omp_after_high,  '-o',   label='One-Shot LRR (After)',color='#00BFFF',linewidth=linewidth, markersize=markersize, )
-# roberta_large.plot(x_axis, acc_test_random_after_high,  '-o',   label='Random LRR (After)',color='#0072BD',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, acc_test_gmp_high_RACE,  '-o', color='magenta',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, acc_test_random_after_high_RACE,  '-o',   color='#0072BD',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, acc_test_omp_after_high_RACE,  '-o',   color='#00BFFF',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, acc_test_gmp_high_RACE,  '-o', color='#CD00CD',linewidth=linewidth, markersize=markersize, )
 
 
-roberta_large.plot(x_axis, acc_test_snip_high_RACE,  '-o', color='#00FF00',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, acc_test_snip_rigl_high_RACE,  '--o', color='#00FF00',linewidth=linewidth, markersize=markersize )
+roberta_large.plot(x_axis, acc_test_snip_high_RACE,  '-o', color='#228B22',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, acc_test_snip_rigl_high_RACE,  '--o', color='#228B22',linewidth=linewidth, markersize=markersize )
 roberta_large.plot(x_axis, acc_test_random_high_RACE,  '-o', color='brown',linewidth=linewidth, markersize=markersize, )
 roberta_large.plot(x_axis, acc_test_random_rigl_high_RACE,  '--o', color='brown',linewidth=linewidth, markersize=markersize)
 roberta_large.plot(x_axis, acc_test_omp_high_RACE,  '-o', color='#bcbd22',linewidth=linewidth, markersize=markersize, )
@@ -344,7 +363,7 @@ roberta_large.spines['top'].set_visible(False)
 
 plt.tight_layout()
 fig.legend(loc='lower center', bbox_to_anchor=(0.0, 0.0, 1, 1), fancybox=False, shadow=False, ncol=6, fontsize=fontsize, frameon=False)
-fig.subplots_adjust(left=0.1 , bottom=0.2, right=0.9, top=0.95, wspace=0.2, hspace=0.2)
+fig.subplots_adjust(left=0.05, bottom=0.2, right=0.95, top=0.95, wspace=0.2, hspace=0.2)
 # roberta_large.set_title('Roberta large on CommonsenseQA',fontsize=fontsize)
 # roberta_large.set_xticks(range(10))
 # roberta_large.set_xticklabels(np.array( [0.2, 0.36, 0.488, 0.590, 0.672, 0.738, 0.791, 0.8325, 0.866, 0.893]), fontsize=10 )
