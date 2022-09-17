c8d7ae61406b2e1704d50caffc2878d1d15f49d4
Allen <39230030+Kyriection@users.noreply.github.com>
2022-09-17 01:01:43 -0500

    Update imp.sh

--- multi_lingual/imp.sh
+++ multi_lingual/imp.sh
@@ -10,7 +10,7 @@ lang_list=examples/multilingual/lang_list.txt  # <path to a file which contains 
 lang_pairs=en-fr,fr-en  #a list language pairs to train multilingual models, e.g. "en-fr,en-cs,fr-en,cs-en"
 # pretrained can be an mBART pretrained model as well
 pretrained_model=examples/multilingual/mbart.cc25.v2/model.pt #<path to a pretrained model>
-
+save_dir=mling_2_2
 
 CUDA_VISIBLE_DEVICES=$1 python train_custom_new.py "$path_2_data" \
     --encoder-normalize-before --decoder-normalize-before \
@@ -26,8 +26,33 @@ CUDA_VISIBLE_DEVICES=$1 python train_custom_new.py "$path_2_data" \
     --lang-pairs "$lang_pairs" \
     --criterion label_smoothed_cross_entropy --label-smoothing 0.2 \
     --optimizer adam --adam-eps 1e-06 --adam-betas '(0.9, 0.98)' \
-    --lr-scheduler inverse_sqrt --lr 3e-05 --warmup-updates 2500 --max-update 40000 \
+    --lr-scheduler inverse_sqrt --lr 3e-05 --warmup-updates 2500 --max-update 10 \
     --dropout 0.3 --attention-dropout 0.1 --weight-decay 0.0 \
     --max-tokens 1024 --update-freq 2 \
     --save-interval 1 --save-interval-updates 5000 --keep-interval-updates 10 --no-epoch-checkpoints \
-    --seed 222 --log-format simple --log-interval 100 --save-dir mling_2_2/0/ --fix --sparse-init iterative_gm --sparsity 0.2 --imp-iters 1
+    --seed 222 --log-format simple --log-interval 100 --save-dir $save_dir/0/ --fix --sparse-init iterative_gm --sparsity 0.2 --imp-iters 1
+
+
+for ((i=1; i<10; i++))
+do
+    j=$((i-1))
+    CUDA_VISIBLE_DEVICES=$1 python train_custom_new.py "$path_2_data" \
+        --encoder-normalize-before --decoder-normalize-before \
+        --arch mbart_large --layernorm-embedding \
+        --task translation_multi_simple_epoch \
+        --restore-file $save_dir/$j/checkpoint_last_iter0.pt \
+        --reset-optimizer --reset-dataloader --reset-meters \
+        --sampling-method "temperature" \
+        --sampling-temperature "1.5" \
+        --encoder-langtok "src" \
+        --decoder-langtok \
+        --lang-dict "$lang_list" \
+        --lang-pairs "$lang_pairs" \
+        --criterion label_smoothed_cross_entropy --label-smoothing 0.2 \
+        --optimizer adam --adam-eps 1e-06 --adam-betas '(0.9, 0.98)' \
+        --lr-scheduler inverse_sqrt --lr 3e-05 --warmup-updates 2500 --max-update 10 \
+        --dropout 0.3 --attention-dropout 0.1 --weight-decay 0.0 \
+        --max-tokens 1024 --update-freq 2 \
+        --save-interval 1 --save-interval-updates 5000 --keep-interval-updates 10 --no-epoch-checkpoints \
+        --seed 222 --log-format simple --log-interval 100 --save-dir $save_dir/$i/ --fix --sparse-init iterative_gm --sparsity 0.2 --imp-iters 1
+done
