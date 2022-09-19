6820afee8bd0c78a3c14f80763015e712275467d
Allen <39230030+Kyriection@users.noreply.github.com>
2022-09-18 21:11:20 -0500

    Update score.sh

--- multi_lingual/score.sh
+++ multi_lingual/score.sh
@@ -1,37 +1,37 @@
-PATH=$1
-Model_idx=$2
+RUN=$1
+PATH=$2
+Model_idx=$3
 src=en
-key=mling22_$2
+key=$1_$3
 
 tgt=cs
-bash multi_lingual/generate22.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${src} ${tgt} ${key}
-bash multi_lingual/generate22.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${tgt} ${src} ${key}
+bash multi_lingual/${RUN}.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${src} ${tgt} ${key}
+bash multi_lingual/${RUN}.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${tgt} ${src} ${key}
 tgt=fr
-bash multi_lingual/generate22.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${src} ${tgt} ${key}
-bash multi_lingual/generate22.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${tgt} ${src} ${key}
+bash multi_lingual/${RUN}.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${src} ${tgt} ${key}
+bash multi_lingual/${RUN}.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${tgt} ${src} ${key}
 tgt=de
-bash multi_lingual/generate22.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${src} ${tgt} ${key}
-bash multi_lingual/generate22.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${tgt} ${src} ${key}
+bash multi_lingual/${RUN}.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${src} ${tgt} ${key}
+bash multi_lingual/${RUN}.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${tgt} ${src} ${key}
 tgt=gu
-bash multi_lingual/generate22.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${src} ${tgt} ${key}
-bash multi_lingual/generate22.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${tgt} ${src} ${key}
+bash multi_lingual/${RUN}.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${src} ${tgt} ${key}
+bash multi_lingual/${RUN}.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${tgt} ${src} ${key}
 tgt=ja
-bash multi_lingual/generate22.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${src} ${tgt} ${key}
-bash multi_lingual/generate22.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${tgt} ${src} ${key}
+bash multi_lingual/${RUN}.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${src} ${tgt} ${key}
+bash multi_lingual/${RUN}.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${tgt} ${src} ${key}
 tgt=my
-bash multi_lingual/generate22.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${src} ${tgt} ${key}
-bash multi_lingual/generate22.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${tgt} ${src} ${key}
+bash multi_lingual/${RUN}.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${src} ${tgt} ${key}
+bash multi_lingual/${RUN}.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${tgt} ${src} ${key}
 tgt=ro
-bash multi_lingual/generate22.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${src} ${tgt} ${key}
-bash multi_lingual/generate22.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${tgt} ${src} ${key}
+bash multi_lingual/${RUN}.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${src} ${tgt} ${key}
+bash multi_lingual/${RUN}.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${tgt} ${src} ${key}
 tgt=ru
-bash multi_lingual/generate22.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${src} ${tgt} ${key}
-bash multi_lingual/generate22.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${tgt} ${src} ${key}
+bash multi_lingual/${RUN}.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${src} ${tgt} ${key}
+bash multi_lingual/${RUN}.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${tgt} ${src} ${key}
 tgt=vi
-bash multi_lingual/generate22.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${src} ${tgt} ${key}
-bash multi_lingual/generate22.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${tgt} ${src} ${key}
+bash multi_lingual/${RUN}.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${src} ${tgt} ${key}
+bash multi_lingual/${RUN}.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${tgt} ${src} ${key}
 tgt=zh
-bash multi_lingual/generate22.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${src} ${tgt} ${key}
-bash multi_lingual/generate22.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${tgt} ${src} ${key}
-
+bash multi_lingual/${RUN}.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${src} ${tgt} ${key}
+bash multi_lingual/${RUN}.sh ${PATH}/${Model_idx}/checkpoint_best_iter0.pt ${tgt} ${src} ${key}
 
