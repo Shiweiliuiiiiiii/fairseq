b498b83fac7afc5f6ae62204876f4196805b5fbe
Allen <39230030+Kyriection@users.noreply.github.com>
2022-09-18 20:34:14 -0500

    Create generate.sh

---
+++ multi_lingual/generate.sh
@@ -0,0 +1,17 @@
+model=$1
+source_lang=$2
+target_lang=$3
+path_2_data=$4
+
+CUDA_VISIBLE_DEVICES=$5 fairseq-generate $path_2_data \
+    --path $model \
+    --task translation_multi_simple_epoch \
+    --gen-subset test \
+    --source-lang $source_lang \
+    --target-lang $target_lang
+    --sacrebleu --remove-bpe 'sentencepiece'\
+    --batch-size 32 \
+    --encoder-langtok "src" \
+    --decoder-langtok \
+    --lang-dict "$lang_list" \
+    --lang-pairs "$lang_pairs" > ${source_lang}_${target_lang}.txt
