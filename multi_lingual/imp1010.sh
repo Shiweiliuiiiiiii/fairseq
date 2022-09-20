376326295d91301a89afb5b38714fb0a3430dc5b
Allen <39230030+Kyriection@users.noreply.github.com>
2022-09-20 08:22:38 -0500

    1

--- multi_lingual/imp1010.sh
+++ multi_lingual/imp1010.sh
@@ -12,28 +12,28 @@ lang_pairs=en-fr,en-cs,en-de,en-gu,en-ja,en-my,en-ro,en-ru,en-vi,en-zh,zh-en,vi-
 pretrained_model=examples/multilingual/mbart.cc25.v2/model.pt #<path to a pretrained model>
 save_dir=mling_10_10
 
-CUDA_VISIBLE_DEVICES=$1 python train_custom_new.py "$path_2_data" \
-    --encoder-normalize-before --decoder-normalize-before \
-    --arch mbart_large --layernorm-embedding \
-    --task translation_multi_simple_epoch \
-    --restore-file "$pretrained_model" \
-    --reset-optimizer --reset-dataloader --reset-meters \
-    --sampling-method "temperature" \
-    --sampling-temperature "1.5" \
-    --encoder-langtok "src" \
-    --decoder-langtok \
-    --lang-dict "$lang_list" \
-    --lang-pairs "$lang_pairs" \
-    --criterion label_smoothed_cross_entropy --label-smoothing 0.2 \
-    --optimizer adam --adam-eps 1e-06 --adam-betas '(0.9, 0.98)' \
-    --lr-scheduler inverse_sqrt --lr 3e-05 --warmup-updates 2500 --max-update 40000 \
-    --dropout 0.3 --attention-dropout 0.1 --weight-decay 0.0 \
-    --max-tokens 1024 --update-freq 2 \
-    --save-interval 1 --save-interval-updates 50000 --keep-interval-updates 10 --no-epoch-checkpoints \
-    --seed 222 --log-format simple --log-interval 100 --save-dir $save_dir/0/ --fix --sparse-init iterative_gm --sparsity 0.2 --imp-iters 0
+# CUDA_VISIBLE_DEVICES=$1 python train_custom_new.py "$path_2_data" \
+#     --encoder-normalize-before --decoder-normalize-before \
+#     --arch mbart_large --layernorm-embedding \
+#     --task translation_multi_simple_epoch \
+#     --restore-file "$pretrained_model" \
+#     --reset-optimizer --reset-dataloader --reset-meters \
+#     --sampling-method "temperature" \
+#     --sampling-temperature "1.5" \
+#     --encoder-langtok "src" \
+#     --decoder-langtok \
+#     --lang-dict "$lang_list" \
+#     --lang-pairs "$lang_pairs" \
+#     --criterion label_smoothed_cross_entropy --label-smoothing 0.2 \
+#     --optimizer adam --adam-eps 1e-06 --adam-betas '(0.9, 0.98)' \
+#     --lr-scheduler inverse_sqrt --lr 3e-05 --warmup-updates 2500 --max-update 40000 \
+#     --dropout 0.3 --attention-dropout 0.1 --weight-decay 0.0 \
+#     --max-tokens 1024 --update-freq 2 \
+#     --save-interval 1 --save-interval-updates 50000 --keep-interval-updates 10 --no-epoch-checkpoints \
+#     --seed 222 --log-format simple --log-interval 100 --save-dir $save_dir/0/ --fix --sparse-init iterative_gm --sparsity 0.2 --imp-iters 0
 
 
-for ((i=1; i<10; i++))
+for ((i=7; i<10; i++))
 do
     j=$((i-1))
     CUDA_VISIBLE_DEVICES=$1 python train_custom_new.py "$path_2_data" \
