17e2feb8bc4b97ccf40df9dc4a19ae786ba1f443
Allen <39230030+Kyriection@users.noreply.github.com>
2022-09-20 20:27:55 -0500

    1

--- read.py
+++ read.py
@@ -11,7 +11,7 @@ def read_bleu(path):
         data = f.readlines()
     data = data[-1]
     key = 'Generate test with beam=5: BLEU4 = '
-    bleu_score = data[len(key): len(key)+10]
+    bleu_score = data[len(key): len(key)+5]
     stop_key = bleu_score.find(',')
     # process_score
     bleu_score = bleu_score[:stop_key]
@@ -21,19 +21,70 @@ def read_bleu(path):
 
 lang_list = ['fr', 'cs', 'de', 'gu', 'ja', 'my', 'ro', 'ru', 'vi', 'zh']
 sparsity = 0
-task = '_1010_'
-
-for lang in lang_list:
-    path = 'en_' + lang + task + str(sparsity) + '.txt'
-    bleu = read_bleu(os.path.join('bleu_result', path))
-    print(path, bleu)
+task = '22'
+stop_key = 1
 
 
+overall_data = {}
+task_list = ['22', '55', '101', '1010']
+stop_key_list = [1,5,10,10]
+for task, stop_key in zip(task_list, stop_key_list):
+    overall_data[task] = {
+        'score_task': [],
+        'score_all': [],
+        'score_task_reverse': [],
+        'score_all_reverse': []
+    }
+    for sparsity in range(6):
+
+        single_sparse_result = []
+        for lang in lang_list:
+            path = 'en_' + lang + '_' + task + '_' + str(sparsity) + '.txt'
+            bleu = read_bleu(os.path.join('bleu_result', path))
+            single_sparse_result.append(bleu)
+        single_sparse_result = np.array(single_sparse_result)
+        score_task = np.mean(single_sparse_result[:stop_key])
+        score_all = np.mean(single_sparse_result)
+
+        single_sparse_result_reverse = []
+        for lang in lang_list:
+            path = 'en_' + lang + '_reverse_' + task + '_' + str(sparsity) + '.txt'
+            bleu = read_bleu(os.path.join('bleu_result', path))
+            single_sparse_result_reverse.append(bleu)
+        single_sparse_result_reverse = np.array(single_sparse_result_reverse)
+        score_task_reverse = np.mean(single_sparse_result_reverse[:stop_key])
+        score_all_reverse = np.mean(single_sparse_result_reverse)
+
+        overall_data[task]['score_task'].append(score_task)
+        overall_data[task]['score_all'].append(score_all)
+        overall_data[task]['score_task_reverse'].append(score_task_reverse)
+        overall_data[task]['score_all_reverse'].append(score_all_reverse)
 
 
+# plot curve 
+# for key in ['score_task', 'score_all', 'score_task_reverse', 'score_all_reverse']:
+#     for task in task_list:
+#         plt.plot(overall_data[task][key], label = task)
+#         plt.legend()
+#         plt.title(key + task)
+#         plt.show()
 
 
+for i, task in enumerate(task_list):
+    plt.subplot(2,2,i+1)
+    result = np.array(overall_data[task]['score_task']) + np.array(overall_data[task]['score_task_reverse'])
+    plt.plot(result, label = task)
+    plt.legend()
+    plt.title(task + 'task')
+plt.show()
 
+for i, task in enumerate(task_list):
+    plt.subplot(2,2,i+1)
+    result = np.array(overall_data[task]['score_all']) + np.array(overall_data[task]['score_all_reverse'])
+    plt.plot(result, label = task)
+    plt.legend()
+    plt.title(task + 'all')
+plt.show()
 
 
 
