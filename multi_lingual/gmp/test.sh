6d393fa16073f674b39364923fc3b3ab7a3f0cb2
Allen <39230030+Kyriection@users.noreply.github.com>
2022-09-23 00:00:15 -0500

    Update test.sh

--- multi_lingual/gmp/test.sh
+++ multi_lingual/gmp/test.sh
@@ -29,4 +29,4 @@ CUDA_VISIBLE_DEVICES=$1 python train.py "$path_2_data" \
     --max-tokens 1024 --update-freq 2 \
     --save-interval 1 --save-interval-updates 50000 --keep-interval-updates 10 --no-epoch-checkpoints \
     --seed 222 --sparse --log-format simple --log-interval 10 --save-dir $save_dir/$state/ --sparse-init dense --sparsity $sparsity \
-    --sparse-mode GMP --prune magnitude --prune-rate 0.5 --growth gradient --update-frequency 5 --redistribution none --batch_size 8
+    --sparse-mode GMP --prune magnitude --prune-rate 0.5 --growth gradient --update-frequency 5 --redistribution none --batch-size 8
