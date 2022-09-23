f640fe868466da6f002ff2daf5c929f00690dc2d
Allen <39230030+Kyriection@users.noreply.github.com>
2022-09-23 00:21:33 -0500

    1

--- multi_lingual/gmp/gmp_2to2.sh
+++ multi_lingual/gmp/gmp_2to2.sh
@@ -29,7 +29,7 @@ CUDA_VISIBLE_DEVICES=$1 python train.py "$path_2_data" \
     --max-tokens 1024 --update-freq 2 \
     --save-interval 1 --save-interval-updates 50000 --keep-interval-updates 10 --no-epoch-checkpoints \
     --seed 222 --sparse --log-format simple --log-interval 100 --save-dir $save_dir/$state/ --sparse-init dense --sparsity $sparsity \
-    --sparse-mode GMP --prune magnitude --prune-rate 0.5 --growth gradient --update-frequency 2000 --redistribution none
+    --sparse-mode GMP_cpu --prune magnitude --prune-rate 0.5 --growth gradient --update-frequency 2000 --redistribution none
 
 
 # Calculate Scores
