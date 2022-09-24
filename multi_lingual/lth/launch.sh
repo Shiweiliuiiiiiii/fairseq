8a9d3a20585ef0f8e79eb1ee626ed39d61efe0dd
Allen <39230030+Kyriection@users.noreply.github.com>
2022-09-24 04:01:51 -0500

    11

--- multi_lingual/lth/launch.sh
+++ multi_lingual/lth/launch.sh
@@ -9,8 +9,10 @@
 # nohup bash multi_lingual/lth/imp_10to1.sh 2 > log_LTH_10to1_c.out 2>&1 &
 # nohup bash multi_lingual/lth/imp_10to10.sh 3 > log_LTH_10to10_c.out 2>&1 &
 
-bash multi_lingual/lth/imp_5to5.sh 1 > log_LTH_5to5_cc.out
-bash multi_lingual/lth/imp_10to10.sh 1 > log_LTH_10to10_cc.out
+# bash multi_lingual/lth/imp_5to5.sh 1 > log_LTH_5to5_cc.out
+# bash multi_lingual/lth/imp_10to10.sh 1 > log_LTH_10to10_cc.out
 
 
+
+nohup bash multi_lingual/lth/imp_10to1.sh 2 > log_LTH_10to1_0924.out 2>&1 &
 
