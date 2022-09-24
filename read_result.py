6d99437186e345e35e58491a6367b689094b1d07
Allen <39230030+Kyriection@users.noreply.github.com>
2022-09-24 16:06:16 -0500

    Update read_result.py

--- read_result.py
+++ read_result.py
@@ -28,13 +28,19 @@ for i in range(11):
 print('Pruning State \t From En to fr, \t cs, \t, de, \t gu, \t ja, \t my, \t ro, \t ru, \t vi, \t zh')
 for i in range(11):
     data = result[i]
-    print('{} \t {}, \t {}, \t {}, \t {}, \t {}, \t {}, \t {}, \t {}, \t {}, \t {}'.format(
+    for lang in ['fr', 'cs', 'de', 'gu', 'ja', 'my', 'ro', 'ru', 'vi', 'zh']:
+        if not 'from_en_to_{}'.format(lang) in data:
+            data['from_en_to_{}'.format(lang)] = 'None'
+    print('{} \t {}, \t {}, \t {}, \t {}, \t {}, \t {}, \t {}, \t {}, \t {}, \t {}'.format(i,
         data['from_en_to_fr'],data['from_en_to_cs'],data['from_en_to_de'],data['from_en_to_gu'],data['from_en_to_ja'],
         data['from_en_to_my'],data['from_en_to_ro'],data['from_en_to_ru'],data['from_en_to_vi'],data['from_en_to_zh']))
 print('Pruning State \t To En from fr, \t cs, \t, de, \t gu, \t ja, \t my, \t ro, \t ru, \t vi, \t zh')
 for i in range(11):
     data = result[i]
-    print('{} \t {}, \t {}, \t {}, \t {}, \t {}, \t {}, \t {}, \t {}, \t {}, \t {}'.format(
+    for lang in ['fr', 'cs', 'de', 'gu', 'ja', 'my', 'ro', 'ru', 'vi', 'zh']:
+        if not 'to_en_from_{}'.format(lang) in data:
+            data['to_en_from_{}'.format(lang)] = 'None'
+    print('{} \t {}, \t {}, \t {}, \t {}, \t {}, \t {}, \t {}, \t {}, \t {}, \t {}'.format(i,
         data['to_en_from_fr'],data['to_en_from_cs'],data['to_en_from_de'],data['to_en_from_gu'],data['to_en_from_ja'],
         data['to_en_from_my'],data['to_en_from_ro'],data['to_en_from_ru'],data['to_en_from_vi'],data['to_en_from_zh']))
         
