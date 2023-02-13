1030a50da0d5546a8bd2026bd666bd1c85101b53
Shiwei Liu <shiwei@v097141.vpn.tue.nl>
2023-02-12 21:57:27 -0600

    obert one-shot pruning and finetuning fix issue

--- fairseq_cli/train.py
+++ fairseq_cli/train.py
@@ -382,6 +382,47 @@ def train(
                 mask.setup_fisher_inverse(trainer, progress)
                 mask.init(model=trainer.model, train_loader=None, device=mask.device, mode=mask.sparse_init,
                           density=(1 - mask.sparsity))
+                progress = progress_bar.progress_bar(
+                    itr,
+                    log_format=cfg.common.log_format,
+                    log_file=cfg.common.log_file,
+                    log_interval=cfg.common.log_interval,
+                    epoch=epoch_itr.epoch,
+                    aim_repo=(
+                        cfg.common.aim_repo
+                        if distributed_utils.is_master(cfg.distributed_training)
+                        else None
+                    ),
+                    aim_run_hash=(
+                        cfg.common.aim_run_hash
+                        if distributed_utils.is_master(cfg.distributed_training)
+                        else None
+                    ),
+                    aim_param_checkpoint_dir=cfg.checkpoint.save_dir,
+                    tensorboard_logdir=(
+                        cfg.common.tensorboard_logdir
+                        if distributed_utils.is_master(cfg.distributed_training)
+                        else None
+                    ),
+                    default_log_format=("tqdm" if not cfg.common.no_progress_bar else "simple"),
+                    wandb_project=(
+                        cfg.common.wandb_project
+                        if distributed_utils.is_master(cfg.distributed_training)
+                        else None
+                    ),
+                    wandb_run_name=os.environ.get(
+                        "WANDB_NAME", os.path.basename(cfg.checkpoint.save_dir)
+                    ),
+                    azureml_logging=(
+                        cfg.common.azureml_logging
+                        if distributed_utils.is_master(cfg.distributed_training)
+                        else False
+                    ),
+                )
+                progress.update_config(_flatten_config(cfg))
+
+                trainer.begin_epoch(epoch_itr.epoch)
+
             elif mask.sparse_mode == 'oBERT':
                 mask.init(model=trainer.model, train_loader=None, device=mask.device, mode=mask.sparse_init,
                           density=(1 - mask.sparsity))
