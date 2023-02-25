5c95032ca9312c86de5b2264d18aefdaa32a0103
Shiwei Liu <shiwei@v095124.vpn.tue.nl>
2023-02-25 11:24:05 -0600

    smc test

--- plot/Camera_reacy_added.py
+++ plot/Camera_reacy_added.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.pyplot import figure
-fig = figure(num=None, figsize=(16, 5), dpi=150, facecolor='w', edgecolor='k')
+fig = figure(num=None, figsize=(10, 5.5), dpi=150, facecolor='w', edgecolor='k')
 fontsize = 12
 Titlesize = 10
 markersize = 7
@@ -49,8 +49,9 @@ robert_lth_csqa = [75.92, 74.04, 69.62, 19.57, 19.57, 19.57, 19.57, 19.57, 19.57
 robert_lth_csqa_noe = [76.57, 74.11, 71.66, 66.17, 51.35, 40.62, 19.57, 19.57, 19.57, 19.57]
 robert_lth_noe_noc  = [76.58, 75.10, 71.58, 66.17, 58.89, 46.52, 38.90, 35.30, 24.73, 22.11]  # reberta_LTH_noemb_nocla_noLRR.out
 robert_lth_noe_noc_LRR = [76.33, 74.44, 72.72, 69.28, 64.29, 58.72, 50.78, 44.96, 39.39, 37.59]
-robert_oBERT_noe_noc_LTT  = [75.51, 75.43, 74.20, 73.05, 72.22, 70.43, 68.80, 67.57, 63.96, 61.18]  # reberta_oBERT_noemb_nocla_LRR.out
-
+robert_oBERT_noe_noc_LRR  = [75.51, 75.43, 74.20, 73.05, 72.22, 70.43, 68.80, 67.57, 63.96, 61.18]  # reberta_oBERT_noemb_nocla_LRR.out
+robert_oBERT_noe_noc_LRR_KD  = [78.21, 77.15, 76.24, 74.93, 72.40, 71.50, 69.87, 68.46, 65.03, 62.41]  # reberta_oBERT_noemb_nocla_LRR.out
+robert_random_after_csqa  = [36.61, 25.06, 21.95, 20.39, 20.88, 20.31, 19.74, 20.80, 21.29, 21.05]
 
 
 roberta_large = fig.add_subplot(1,1,1)
@@ -61,11 +62,10 @@ roberta_large.plot(x_axis, robert_gmp_csqa_5epochs,  '-o',   label='GMP',color='
 roberta_large.plot(x_axis, robert_lth_csqa,  '-o',   label='LTH',color='orange',linewidth=linewidth, markersize=markersize, )
 roberta_large.plot(x_axis, robert_lth_csqa_noe,  '-o',   label='LTH (not pruning emb)',color='brown',linewidth=linewidth, markersize=markersize, )
 roberta_large.plot(x_axis, robert_lth_noe_noc,  '-o',   label='LTH (not pruning emb and classifier)',color='#bcbd22',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, robert_lth_noe_noc_LRR,  '-o',   label='LTH + LRR (not pruning emb and classifier)',color='blue',linewidth=linewidth, markersize=markersize, )
-# roberta_large.plot(x_axis, robert_gmp_105_600_no_embed_no_classifier_10epochs,  '-o',   label='GMP (no emb and no classifier) 10 epochs',color='orange',linewidth=linewidth, markersize=markersize, )
-# roberta_large.plot(x_axis, robert_gmp_LLR_no_embed_no_classifier_5epochs,  '-o',   label='GMP + LRR (not pruning emb and classifier)',color='purple',linewidth=linewidth, markersize=markersize, )
-# roberta_large.plot(x_axis, robert_obert_LLR_no_embed_no_classifier_5epochs_noKD,  '--o',   label='oBERT - KD (not pruning emb and classifier)',color='red',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, robert_oBERT_noe_noc_LTT,  '-o',   label='oBERT LRR (not pruning emb and classifier)',color='red',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, robert_lth_noe_noc_LRR,  '-o',   label='LTH + LRR (not pruning emb and classifier)',color='#00BFFF',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, robert_random_after_csqa,  '-o',   label='Random (After)',color='#0072BD',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, robert_oBERT_noe_noc_LRR,  '-o',   label='oBERT + LRR (not pruning emb and classifier)',color='pink',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, robert_oBERT_noe_noc_LRR_KD,  '-o',   label='oBERT + LRR + KD (not pruning emb and classifier)',color='purple',linewidth=linewidth, markersize=markersize, )
 # roberta_large.plot(x_axis, oBERT_oneshot_ft_noembed_nocla,  '-o',   label='oBERT After (not pruning emb and classifier)',color='red',linewidth=linewidth, markersize=markersize, )
 # # roberta_large.plot(x_axis, robert_structured,  '-o',   label='Structrued (L1 Norm)',color='orange',linewidth=linewidth, markersize=markersize, )
 
@@ -79,7 +79,7 @@ roberta_large.plot(x_axis, robert_oBERT_noe_noc_LTT,  '-o',   label='oBERT LRR (
 # # roberta_large.plot(x_axis, robert_structured,  '-o',   label='Structrued (L1 Norm)',color='orange',linewidth=linewidth, markersize=markersize, )
 #
 
-roberta_large.set_title('CommonsenseQA (Roberta Large no emb and no classifier)',fontsize=Titlesize)
+roberta_large.set_title('Roberta-Large on CSQA',fontsize=Titlesize)
 roberta_large.axes.get_xaxis().set_visible(True)
 roberta_large.set_ylabel('Accuracy [%]', fontsize=Titlesize)
 roberta_large.set_xlabel('Sparsity', fontsize=Titlesize)
@@ -99,7 +99,7 @@ roberta_large.spines['top'].set_visible(False)
 
 
 plt.tight_layout()
-fig.legend(loc='lower center', bbox_to_anchor=(0.0, 0.0, 1, 1), fancybox=False, shadow=False, ncol=4, fontsize=fontsize, frameon=False)
-fig.subplots_adjust(left=0.05, bottom=0.29, right=0.95, top=0.94, wspace=0.2, hspace=0.35)
-plt.savefig('GMP_10_epochs.pdf', bbox_inches='tight')
+fig.legend(loc='lower center', bbox_to_anchor=(0.0, 0.0, 1, 1), fancybox=False, shadow=False, ncol=5, fontsize=fontsize, frameon=False)
+fig.subplots_adjust(left=0.05, bottom=0.32, right=0.95, top=0.94, wspace=0.2, hspace=0.35)
+plt.savefig('Investigation.pdf', bbox_inches='tight')
 plt.show()
--- plot/Sparsity_commonsense_qa_CMR.py
+++ plot/Sparsity_commonsense_qa_CMR.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from matplotlib.pyplot import figure
-fig = figure(num=None, figsize=(16, 10), dpi=150, facecolor='w', edgecolor='k')
+fig = figure(num=None, figsize=(16, 5), dpi=150, facecolor='w', edgecolor='k')
 fontsize = 12
 Titlesize = 12
 markersize = 1
@@ -25,8 +25,6 @@ with open('/Users/shiwei/Projects/fairseq/results/reasoning/sparsity/cobert_race
                 else:
                     full_name.append(single_name)
 
-
-
 csqa_sparsity = np.array(csqa_sparsity).reshape(7, -1)
 csqa_sparsity_gm = csqa_sparsity[0].reshape(10,-1)
 csqa_sparsity_gm_after = csqa_sparsity[1].reshape(10,-1)
@@ -39,6 +37,8 @@ csqa_sparsity_snip= csqa_sparsity[6].reshape(10,-1)
 #0.2 0.36 0.488 0.590 0.672 0.738 0.791 0.8325 0.866 0.893
 csqa_sparsity_gm_02 = csqa_sparsity_gm[0]
 csqa_sparsity_gm_036 = csqa_sparsity_gm[1]
+
+
 csqa_sparsity_gm_0488 = csqa_sparsity_gm[2]
 csqa_sparsity_gm_059 = csqa_sparsity_gm[3]
 csqa_sparsity_gm_0672 = csqa_sparsity_gm[4]
@@ -62,17 +62,7 @@ picked_names = ['embed_tokens.weight', 'embed_positions.weight', 'layers.1.in_pr
 'layers.17.in_proj_weight',  'layers.18.in_proj_weight', 'layers.19.in_proj_weight', \
 'layers.20.in_proj_weight',  'layers.21.in_proj_weight', 'layers.22.in_proj_weight', 'layers.22.in_proj_weight', \
                 'model.classification_heads.sentence_classification_head.dense.weight',]
-print(full_name)
-for i in range(len(full_name)):
-    if full_name[i] not in picked_names:
-        full_name[i] = ''
-    elif 'layers' in full_name[i]:
-        full_name[i] = full_name[i].replace('layers', 'L')
-    elif 'sentence_classification_head' in full_name[i]:
-        full_name[i] = 'classification_heads'
 
-full_name[1] = ''
-full_name[2] = 'embed_positions.weight'
 
 
 print(full_name)
@@ -241,74 +231,188 @@ csqa_sparsity_snip_0893 = csqa_sparsity_snip[9]
 # roberta_large.spines['right'].set_visible(False)
 # roberta_large.spines['top'].set_visible(False)
 
+csqa_sparsity_LTH_noemb_nocla = []
+full_name_LTH_noemb_nocla = []
+with open('/Users/shiwei/Projects/fairseq/roberta_LTH_noembed_noclassifier.out') as file:
+    for line in file:
+        if 'sparsity of' in line and 'torch.Size' in line:
+            single_name = line.split()[-3]
 
-x_axis = range(len(full_name))
-roberta_large = fig.add_subplot(3,1,1)
-roberta_large.plot(x_axis, np.array(csqa_sparsity_IMP_036),  '-o',  label='LTH (After)', color='orange',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, np.array(csqa_sparsity_gm_036),  '--o',  label='OMP (Before)', color='#00BFFF',linewidth=linewidth, markersize=markersize, )
-# roberta_large.plot(x_axis, np.array(csqa_sparsity_random_after_36),  '-o', color='#0072BD',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, np.array(csqa_sparsity_gmp_036   ),  '-o', label='GMP (During)', color='#CD00CD',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, np.array(csqa_sparsity_snip_036),  '-o',  label='SNIP (Before)', color='#228B22',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, np.array(csqa_sparsity_random_036),  '-o', label='Random (Before)', color='brown',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, np.array(csqa_sparsity_gm_after_036),  '-o', label='OMP (After)', color='#00BFFF',linewidth=linewidth, markersize=markersize, )
+            if single_name not in removed_layer:
+                csqa_sparsity_LTH_noemb_nocla.append(float(line.split()[-1]))
+                if 'encoder.' in single_name:
+                    full_name_LTH_noemb_nocla.append(single_name.split('encoder.')[-1])
 
+                else:
+                    full_name_LTH_noemb_nocla.append(single_name)
 
-
-roberta_large.set_ylabel('Layerwise Sparsity', fontsize=fontsize)
-roberta_large.set_title('RoBERTa on RACE, Overall Sparity=36%',fontsize=Titlesize)
-roberta_large.yaxis.set_ticks([0.0, 0.2, 0.4, 0.6, 0.8, 1.0], labelsize=fontsize)
-roberta_large.xaxis.set_ticks(x_axis, rotation=45)
-roberta_large.set_xticklabels(['']*len(x_axis), rotation=90, fontsize=1)
-roberta_large.tick_params(axis='both', which='major', labelsize=fontsize)
-roberta_large.axes.get_xaxis().set_visible(False)
-roberta_large.grid(True, axis='y',  linestyle='-', linewidth=0.5, )
-roberta_large.spines['right'].set_visible(False)
-roberta_large.spines['top'].set_visible(False)
+# print(len(np.array(csqa_sparsity_LTH_LRR_noemb_nocla).reshape(10, -1)[1]))
 
 
+csqa_sparsity_LTH_LRR_noemb_nocla = []
+full_name_LTH_LRR_noemb_nocla = []
+with open('/Users/shiwei/Projects/fairseq/roberta_LTH_LLR_noembed_noclassifier.out') as file:
+    for line in file:
+        if 'sparsity of' in line and 'torch.Size' in line:
+            single_name = line.split()[3]
 
-x_axis = range(len(full_name))
-roberta_large = fig.add_subplot(3,1,2)
-roberta_large.plot(x_axis, np.array(csqa_sparsity_IMP_0672),  '-o', color='orange',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, np.array(csqa_sparsity_gm_0672),  '-o', color='#00BFFF',linewidth=linewidth, markersize=markersize, )
-# roberta_large.plot(x_axis, np.array(csqa_sparsity_random_after_672),  '-o', color='#0072BD',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, np.array(csqa_sparsity_gmp_0672),  '-o', color='#CD00CD',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, np.array(csqa_sparsity_snip_0672),  '-o', color='#228B22',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, np.array(csqa_sparsity_random_0672),  '-o', color='brown',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, np.array(csqa_sparsity_gm_after_0672),  '-o',  color='#00BFFF',linewidth=linewidth, markersize=markersize, )
+            if single_name not in removed_layer:
+                csqa_sparsity_LTH_LRR_noemb_nocla.append(float(line.split()[-1]))
+                if 'encoder.' in single_name:
+                    full_name_LTH_LRR_noemb_nocla.append(single_name.split('encoder.')[-1])
+                else:
+                    full_name_LTH_LRR_noemb_nocla.append(single_name)
 
-
-
-roberta_large.set_ylabel('Layerwise Sparsity', fontsize=fontsize)
-roberta_large.set_title('Roberta on RACE, Overall Sparity=67%',fontsize=Titlesize)
-roberta_large.yaxis.set_ticks([0.0, 0.2, 0.4, 0.6, 0.8, 1.0], labelsize=fontsize)
-roberta_large.xaxis.set_ticks(x_axis, rotation=45)
-roberta_large.set_xticklabels(['']*len(x_axis), rotation=90, fontsize=1)
-roberta_large.tick_params(axis='both', which='major', labelsize=fontsize)
-roberta_large.axes.get_xaxis().set_visible(False)
-roberta_large.grid(True, axis='y',  linestyle='-', linewidth=0.5, )
-roberta_large.spines['right'].set_visible(False)
-roberta_large.spines['top'].set_visible(False)
+print(len(np.array(csqa_sparsity_LTH_LRR_noemb_nocla).reshape(10, -1)[1]))
 
 
-x_axis = range(len(full_name))
-roberta_large = fig.add_subplot(3,1,3)
-roberta_large.plot(x_axis, np.array(csqa_sparsity_IMP_08325),  '-o', color='orange',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, np.array(csqa_sparsity_gm_08325),  '-o', color='#00BFFF',linewidth=linewidth, markersize=markersize, )
-# roberta_large.plot(x_axis, np.array(csqa_sparsity_random_after_832),  '-o', color='#0072BD',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, np.array(csqa_sparsity_gmp_08325),  '-o', color='#CD00CD',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, np.array(csqa_sparsity_snip_08325),  '-o', color='#228B22',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, np.array(csqa_sparsity_random_08325),  '-o', color='brown',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, np.array(csqa_sparsity_gm_after_08325),  '-o',  color='#00BFFF',linewidth=linewidth, markersize=markersize, )
+csqa_sparsity_oBERT_KD_LRR_noemb_nocla = []
+full_name_oBERT_LRR_noemb_nocla = []
+with open('/Users/shiwei/Projects/fairseq/roberta_oBERT_LLR_KD_noembed_noclassifier.txt') as file:
+    for line in file:
+        if 'sparsity of' in line and 'torch.Size' in line:
+            single_name = line.split()[-3]
 
+            if single_name not in removed_layer:
+                csqa_sparsity_oBERT_KD_LRR_noemb_nocla.append(float(line.split()[-1]))
+                if 'encoder.' in single_name:
+                    full_name_oBERT_LRR_noemb_nocla.append(single_name.split('encoder.')[-1])
+                else:
+                    full_name_oBERT_LRR_noemb_nocla.append(single_name)
+
+# csqa_sparsity_oBERT_KD_LRR_noemb_nocla = len(np.array(csqa_sparsity_oBERT_KD_LRR_noemb_nocla).reshape(10, -1)[1])
+
+# full_name_LTH_noemb_nocla = full_name_LTH_noemb_nocla[:144]
+# print(np.array(full_name_LTH_LRR_noemb_nocla).reshape(10, -1)[1])
+
+
+csqa_sparsity_oBERT_LLR_noemb_nocla = []
+full_name_oBERT_LLR_noemb_nocla = []
+with open('/Users/shiwei/Projects/fairseq/roberta_oBERT_LLR_noembed_noclassifier.out') as file:
+    for line in file:
+        if 'sparsity of' in line and 'torch.Size' in line:
+            single_name = line.split()[-3]
+            if single_name not in removed_layer:
+                csqa_sparsity_oBERT_LLR_noemb_nocla.append(float(line.split()[-1]))
+                if 'encoder.' in single_name:
+                    full_name_oBERT_LLR_noemb_nocla.append(single_name.split('encoder.')[-1])
+                else:
+                    full_name_oBERT_LLR_noemb_nocla.append(single_name)
+
+# csqa_sparsity_oBERT_LLR_noemb_nocla = len(np.array(csqa_ sparsity_oBERT_LLR_noemb_nocla).reshape(10, -1)[1])
+
+# full_name_LTH_noemb_nocla = full_name_LTH_noemb_nocla[:144]
+# print(np.array(full_name_LTH_LRR_noemb_nocla).reshape(10, -1)[1])
+
+
+full_name_LTH_LRR_noemb_nocla = full_name_LTH_LRR_noemb_nocla[:144]
+
+# print(full_name_LTH_LRR_noemb_nocla)
+for i in range(len(full_name_LTH_LRR_noemb_nocla)):
+    # if 'self_attn' in full_name_LTH_LRR_noemb_nocla[i]:
+    #     full_name_LTH_LRR_noemb_nocla[i] = ''
+    if 'layers' in full_name_LTH_LRR_noemb_nocla[i]:
+        full_name_LTH_LRR_noemb_nocla[i] = full_name_LTH_LRR_noemb_nocla[i].replace('layers', 'L')
+    elif 'sentence_classification_head' in full_name_LTH_LRR_noemb_nocla[i]:
+        full_name_LTH_LRR_noemb_nocla[i] = 'classification_heads'
+
+# full_name_LTH_LRR_noemb_nocla[1] = ''
+full_name_LTH_LRR_noemb_nocla[2] = 'embed_positions.weight'
+print(full_name_LTH_LRR_noemb_nocla)
+
+
+x_axis = range(144)
+roberta_large = fig.add_subplot(1,1,1)
+roberta_large.plot(x_axis, np.array(csqa_sparsity_LTH_noemb_nocla).reshape(10, -1)[1],  '-o',  label='LTH (not pruning emb and classifier)', color='#228B22',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, np.array(csqa_sparsity_LTH_LRR_noemb_nocla).reshape(10, -1)[1],  '-o',  label='LTH + LRR (not pruning emb and classifier)', color='orange',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, np.array(csqa_sparsity_oBERT_KD_LRR_noemb_nocla).reshape(10, -1)[1],  '-o',  label='oBERT + LRR + KD (not pruning emb and classifier)', color='#00BFFF',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, np.array(csqa_sparsity_oBERT_LLR_noemb_nocla).reshape(10, -1)[1],  '-o',  label='oBERT + LRR (not pruning emb and classifier)', color='pink',linewidth=linewidth, markersize=markersize, )
+# roberta_large.plot(x_axis, np.array(csqa_sparsity_gm_036),  '--o',  label='OMP (Before)', color='#00BFFF',linewidth=linewidth, markersize=markersize, )
+# # roberta_large.plot(x_axis, np.array(csqa_sparsity_random_after_36),  '-o', color='#0072BD',linewidth=linewidth, markersize=markersize, )
+# roberta_large.plot(x_axis, np.array(csqa_sparsity_gmp_036   ),  '-o', label='GMP (During)', color='#CD00CD',linewidth=linewidth, markersize=markersize, )
+# roberta_large.plot(x_axis, np.array(csqa_sparsity_snip_036),  '-o',  label='SNIP (Before)', color='#228B22',linewidth=linewidth, markersize=markersize, )
+# roberta_large.plot(x_axis, np.array(csqa_sparsity_random_036),  '-o', label='Random (Before)', color='brown',linewidth=linewidth, markersize=markersize, )
+# roberta_large.plot(x_axis, np.array(csqa_sparsity_gm_after_036),  '-o', label='OMP (After)', color='#00BFFF',linewidth=linewidth, markersize=markersize, )
+
+# roberta_large.set_ylabel('Layerwise Sparsity', fontsize=fontsize)
+# roberta_large.set_title('RoBERTa on RACE, Overall Sparity=36%',fontsize=Titlesize)
+# roberta_large.yaxis.set_ticks([0.0, 0.2, 0.4, 0.6, 0.8, 1.0], labelsize=fontsize)
+# roberta_large.xaxis.set_ticks(x_axis, rotation=45)
+# roberta_large.set_xticklabels(['']*len(x_axis), rotation=90, fontsize=1)
+# roberta_large.tick_params(axis='both', which='major', labelsize=fontsize)
+# roberta_large.axes.get_xaxis().set_visible(False)
+# roberta_large.grid(True, axis='y',  linestyle='-', linewidth=0.5, )
+# roberta_large.spines['right'].set_visible(False)
+# roberta_large.spines['top'].set_visible(False)
+
+
+# x_axis = range(len(full_name))
+# roberta_large = fig.add_subplot(3,1,1)
+# roberta_large.plot(x_axis, np.array(csqa_sparsity_IMP_036),  '-o',  label='LTH (After)', color='orange',linewidth=linewidth, markersize=markersize, )
+# roberta_large.plot(x_axis, np.array(csqa_sparsity_gm_036),  '--o',  label='OMP (Before)', color='#00BFFF',linewidth=linewidth, markersize=markersize, )
+# # roberta_large.plot(x_axis, np.array(csqa_sparsity_random_after_36),  '-o', color='#0072BD',linewidth=linewidth, markersize=markersize, )
+# roberta_large.plot(x_axis, np.array(csqa_sparsity_gmp_036   ),  '-o', label='GMP (During)', color='#CD00CD',linewidth=linewidth, markersize=markersize, )
+# roberta_large.plot(x_axis, np.array(csqa_sparsity_snip_036),  '-o',  label='SNIP (Before)', color='#228B22',linewidth=linewidth, markersize=markersize, )
+# roberta_large.plot(x_axis, np.array(csqa_sparsity_random_036),  '-o', label='Random (Before)', color='brown',linewidth=linewidth, markersize=markersize, )
+# roberta_large.plot(x_axis, np.array(csqa_sparsity_gm_after_036),  '-o', label='OMP (After)', color='#00BFFF',linewidth=linewidth, markersize=markersize, )
+#
+#
+#
+# roberta_large.set_ylabel('Layerwise Sparsity', fontsize=fontsize)
+# roberta_large.set_title('RoBERTa on RACE, Overall Sparity=36%',fontsize=Titlesize)
+# roberta_large.yaxis.set_ticks([0.0, 0.2, 0.4, 0.6, 0.8, 1.0], labelsize=fontsize)
+# roberta_large.xaxis.set_ticks(x_axis, rotation=45)
+# roberta_large.set_xticklabels(['']*len(x_axis), rotation=90, fontsize=1)
+# roberta_large.tick_params(axis='both', which='major', labelsize=fontsize)
+# roberta_large.axes.get_xaxis().set_visible(False)
+# roberta_large.grid(True, axis='y',  linestyle='-', linewidth=0.5, )
+# roberta_large.spines['right'].set_visible(False)
+# roberta_large.spines['top'].set_visible(False)
+
+
+#
+# x_axis = range(len(full_name))
+# roberta_large = fig.add_subplot(3,1,2)
+# roberta_large.plot(x_axis, np.array(csqa_sparsity_IMP_0672),  '-o', color='orange',linewidth=linewidth, markersize=markersize, )
+# roberta_large.plot(x_axis, np.array(csqa_sparsity_gm_0672),  '-o', color='#00BFFF',linewidth=linewidth, markersize=markersize, )
+# # roberta_large.plot(x_axis, np.array(csqa_sparsity_random_after_672),  '-o', color='#0072BD',linewidth=linewidth, markersize=markersize, )
+# roberta_large.plot(x_axis, np.array(csqa_sparsity_gmp_0672),  '-o', color='#CD00CD',linewidth=linewidth, markersize=markersize, )
+# roberta_large.plot(x_axis, np.array(csqa_sparsity_snip_0672),  '-o', color='#228B22',linewidth=linewidth, markersize=markersize, )
+# roberta_large.plot(x_axis, np.array(csqa_sparsity_random_0672),  '-o', color='brown',linewidth=linewidth, markersize=markersize, )
+# roberta_large.plot(x_axis, np.array(csqa_sparsity_gm_after_0672),  '-o',  color='#00BFFF',linewidth=linewidth, markersize=markersize, )
+#
+#
+#
+# roberta_large.set_ylabel('Layerwise Sparsity', fontsize=fontsize)
+# roberta_large.set_title('Roberta on RACE, Overall Sparity=67%',fontsize=Titlesize)
+# roberta_large.yaxis.set_ticks([0.0, 0.2, 0.4, 0.6, 0.8, 1.0], labelsize=fontsize)
+# roberta_large.xaxis.set_ticks(x_axis, rotation=45)
+# roberta_large.set_xticklabels(['']*len(x_axis), rotation=90, fontsize=1)
+# roberta_large.tick_params(axis='both', which='major', labelsize=fontsize)
+# roberta_large.axes.get_xaxis().set_visible(False)
+# roberta_large.grid(True, axis='y',  linestyle='-', linewidth=0.5, )
+# roberta_large.spines['right'].set_visible(False)
+# roberta_large.spines['top'].set_visible(False)
+#
+#
+# x_axis = range(len(full_name))
+# roberta_large = fig.add_subplot(3,1,3)
+# roberta_large.plot(x_axis, np.array(csqa_sparsity_IMP_08325),  '-o', color='orange',linewidth=linewidth, markersize=markersize, )
+# roberta_large.plot(x_axis, np.array(csqa_sparsity_gm_08325),  '-o', color='#00BFFF',linewidth=linewidth, markersize=markersize, )
+# # roberta_large.plot(x_axis, np.array(csqa_sparsity_random_after_832),  '-o', color='#0072BD',linewidth=linewidth, markersize=markersize, )
+# roberta_large.plot(x_axis, np.array(csqa_sparsity_gmp_08325),  '-o', color='#CD00CD',linewidth=linewidth, markersize=markersize, )
+# roberta_large.plot(x_axis, np.array(csqa_sparsity_snip_08325),  '-o', color='#228B22',linewidth=linewidth, markersize=markersize, )
+# roberta_large.plot(x_axis, np.array(csqa_sparsity_random_08325),  '-o', color='brown',linewidth=linewidth, markersize=markersize, )
+# roberta_large.plot(x_axis, np.array(csqa_sparsity_gm_after_08325),  '-o',  color='#00BFFF',linewidth=linewidth, markersize=markersize, )
+#
 
 
 roberta_large.set_ylabel('Layerwise Sparsity', fontsize=fontsize)
 # roberta_large.set_xlabel('Sparsity',fontsize=fontsize)
-roberta_large.set_title('RoBERTa on RACE, Overall Sparity=83%',fontsize=Titlesize)
+roberta_large.set_title('RoBERTa on CSQA, Overall Sparity=36%',fontsize=Titlesize)
 roberta_large.yaxis.set_ticks([0.0, 0.2, 0.4, 0.6, 0.8, 1.0], labelsize=fontsize)
 roberta_large.xaxis.set_ticks(x_axis, rotation=90, fontsize=1)
-roberta_large.set_xticklabels(full_name, rotation=90, fontsize=7)
+roberta_large.set_xticklabels(full_name_LTH_LRR_noemb_nocla[:144], rotation=90, fontsize=3)
 # roberta_large.tick_params(axis='both', which='major', labelsize=5)
 roberta_large.axes.get_xaxis().set_visible(True)
 roberta_large.grid(True, axis='y', linestyle='-', linewidth=0.5, )
@@ -316,8 +420,8 @@ roberta_large.spines['right'].set_visible(False)
 roberta_large.spines['top'].set_visible(False)
 
 plt.tight_layout()
-fig.legend(loc='lower center', bbox_to_anchor=(0.0, 0.0, 1, 1), fancybox=False, shadow=False, ncol=6, fontsize=fontsize, frameon=False)
-fig.subplots_adjust(left=0.05, bottom=0.15, right=0.95, top=0.95, wspace=0.2, hspace=0.20)
+fig.legend(loc='lower center', bbox_to_anchor=(0.0, 0.0, 1, 1), fancybox=False, shadow=False, ncol=2, fontsize=8, frameon=False)
+fig.subplots_adjust(left=0.05, bottom=0.29, right=0.95, top=0.94, wspace=0.2, hspace=0.35)
 
-plt.savefig('layerwise_sparsity_winogrande.pdf')
+plt.savefig('layerwise_sparsity_winogrande_roadmap.png')
 plt.show()
